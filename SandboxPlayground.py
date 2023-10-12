be2bac5a33ae6e2041c7227f6651fbb2dfdcd12c
fere122 <06blake.krueger@gmail.com>
2023-10-12 08:27:05 -0400

    Add print lines introducing my self

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -1,3 +1,6 @@
 #Developer: Blake, Krueger
 # Date 10,21,23
 # Program, Sandbox Playground
+
+print("Hello Mr. Lange!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+print("\nI am looking forward to buying stuff for PC")
